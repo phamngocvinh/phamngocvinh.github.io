ef5487614df7f7389703ee287bb90a87a40b430f
Pham Ngoc Vinh <phamngocvinh@live.com>
2022-03-30 14:25:02 +0700

    Update script.js

--- cv/script.js
+++ cv/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
   var year = new Date().getFullYear() - 2015;
   var word =
-    'A passionate Software Engineer with ' +
+    "Hi, I'm Vinh. I've worked in software engineer with over " +
     year +
-    '+ years of experience in a Japanese Company';
-  $('#introduce').text(word)
+    " experience developing, testing and creating program document for Japanese Company." +
+    "I am a fast learner as I always find interesting in learning new things." +
+    "In my free time, I like to watch science video both to learn and to improve my self.";
+  $("#introduce").text(word);
 });
